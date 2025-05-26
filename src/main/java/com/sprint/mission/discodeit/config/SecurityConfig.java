1bb60e44cc84bf4465b374a51cbb6d5ac340dfd8
yeongun <gjduddns@gmail.com>
2025-05-26 11:20:42 +0900

    chore: SecurityConfig에 api로 시작하지 않는 요청은 모두 허용하도록 수정

--- src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
+++ src/main/java/com/sprint/mission/discodeit/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.stream.IntStream;
@@ -16,6 +17,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(SecurityMatchers.NON_API).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .logout(AbstractHttpConfigurer::disable); // 로그아웃 관련 필터 제외
+
         return http.build();
     }
 
